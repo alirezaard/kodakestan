779e309b95fab8c79a7e2faa8c2a4d6cd8c7eb22
Alireza <alireza.ard@gmail.com>
2020-06-17 16:57:14 +0430

    all changes until 6/17/2020

--- src/main/java/com/kodakestan/kodak/configuration/web/ThymeleafConfiguration.java
+++ src/main/java/com/kodakestan/kodak/configuration/web/ThymeleafConfiguration.java
@@ -1,37 +1,37 @@
-package com.kodakestan.kodak.configuration.web;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-
-@Configuration
-@EnableWebMvc
-public class ThymeleafConfiguration {
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(thymeleafTemplateResolver());
-        return templateEngine;
-    }
-
-    @Bean
-    public SpringResourceTemplateResolver thymeleafTemplateResolver() {
-        SpringResourceTemplateResolver templateResolver
-                = new SpringResourceTemplateResolver();
-        templateResolver.setPrefix("/templates/");
-//        templateResolver.setPrefix("/WEB-INF/views/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode("HTML5");
-        return templateResolver;
-    }
-
-    @Bean
-    public ThymeleafViewResolver thymeleafViewResolver() {
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setTemplateEngine(templateEngine());
-        return viewResolver;
-    }
-}
+//package com.kodakestan.kodak.configuration.web;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.thymeleaf.spring5.SpringTemplateEngine;
+//import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+//import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+//
+//@Configuration
+//@EnableWebMvc
+//public class ThymeleafConfiguration {
+//    @Bean
+//    public SpringTemplateEngine templateEngine() {
+//        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+//        templateEngine.setTemplateResolver(thymeleafTemplateResolver());
+//        return templateEngine;
+//    }
+//
+//    @Bean
+//    public SpringResourceTemplateResolver thymeleafTemplateResolver() {
+//        SpringResourceTemplateResolver templateResolver
+//                = new SpringResourceTemplateResolver();
+//        templateResolver.setPrefix("/templates/");
+////        templateResolver.setPrefix("/WEB-INF/views/");
+//        templateResolver.setSuffix(".html");
+//        templateResolver.setTemplateMode("HTML5");
+//        return templateResolver;
+//    }
+//
+//    @Bean
+//    public ThymeleafViewResolver thymeleafViewResolver() {
+//        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+//        viewResolver.setTemplateEngine(templateEngine());
+//        return viewResolver;
+//    }
+//}
